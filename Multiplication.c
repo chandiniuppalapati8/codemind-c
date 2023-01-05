fdbc57c148b26724755d2ed1cc5bb464337bc926
chandiniuppalapati8 <119730600+chandiniuppalapati8@users.noreply.github.com>
2023-01-05 14:24:52 +0530

    #include <stdio.h> int main() {     double a, b, product;     scanf("%lf %lf", &a, &b);
        // Calculating product
        product = a * b;
    
        // %.2lf displays number up to 2 decimal point
        printf("%.0lf", product);
        
        return 0;
    }

---
+++ Multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    double a, b, product;
+    scanf("%lf %lf", &a, &b);  
+ 
+    // Calculating product
+    product = a * b;
+
+    // %.2lf displays number up to 2 decimal point
+    printf("%.0lf", product);
+    
+    return 0;
+}
