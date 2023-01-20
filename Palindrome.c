35761605687930db0f4c0df8de0ffee1ad380f53
chandiniuppalapati8 <119730600+chandiniuppalapati8@users.noreply.github.com>
2023-01-20 14:54:02 +0530

    #include<stdio.h> int main() { int n,r,sum=0,temp; scanf("%d",&n); temp=n; while(n>0) { r=n%10; sum=(sum*10)+r; n=n/10; } if(temp==sum) printf("Palindrome "); else printf("Not Palindrome"); return 0; }

--- Palindrome.c
+++ Palindrome.c
@@ -1,24 +1,18 @@
-#include <stdio.h>
-int main() 
-{
-  int n, reversed = 0,rem, original;
-    scanf("%d", &n);
-    original = n;
-    while (n != 0) 
-    {
-        rem = n % 10;
-        reversed = reversed * 10 + rem;
-        n /= 10;
-    }
-    if (original == reversed)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("Palindrome ");    
+else    
+printf("Not Palindrome");   
+return 0;  
 }
-
-
-   
